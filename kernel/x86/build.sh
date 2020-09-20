d5cce15d599625021e73a99cd3cbc4370d20da9f
coderarjob <mukherjeearjob@gmail.com>
2020-09-20 16:54:29 +0530

    Builds kernel.c as a 32bit i386 program.

--- kernel/x86/build.sh
+++ kernel/x86/build.sh
@@ -7,7 +7,13 @@
 
 # Build the Kernel
 echo "    [ Compilling Kernel ]    "
-nasm -f bin kernel/x86/kernel.s \
-    -I kernel/x86 $NASM_INCPATH \
-    -O0 -o $OBJDIR/kernel.flt \
-    -l $LISTDIR/kernel.lst || exit
+#nasm -f bin kernel/x86/kernel.s \
+#   -I kernel/x86 $NASM_INCPATH \
+#   -O0 -o $OBJDIR/kernel.flt \
+#   -l $LISTDIR/kernel.lst || exit
+
+$GCC32 -c kernel/x86/kernel.c -o $OBJDIR/kernel.o
+$GCC32 -S kernel/x86/kernel.c -o $LISTDIR/kernel.lst
+
+$LD_KERNEL $OBJDIR/kernel.o -o $OBJDIR/kernel.elf
+objcopy -O binary $OBJDIR/kernel.elf $OBJDIR/kernel.flt
