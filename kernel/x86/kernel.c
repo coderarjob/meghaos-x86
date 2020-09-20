6bd53601f68519d46865cdda4c00c92031a97085
coderarjob <mukherjeearjob@gmail.com>
2020-09-20 16:53:39 +0530

    Basic test kernel with printstring and printhex. Fully 32 bit program

---
+++ kernel/x86/kernel.c
@@ -0,0 +1,22 @@
+#include <ints.h>
+
+void prints(char *s);
+void printhex(u16 num);
+
+void __main()
+{
+    prints("Kernel ready..");
+    printhex(0xbabe);
+    while(1);
+}
+
+void prints(char *s)
+{
+    u8 *vgabuff = (u8 *)0xb8000;
+
+    for(; *s; s++, vgabuff+=2) {
+        *vgabuff = *s;
+        *(vgabuff+1) = 0xF;
+    }
+}
+
