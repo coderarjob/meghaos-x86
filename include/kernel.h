bb842c3d194d3f2d7f459498c868ca2fd95de146
coderarjob <mukherjeearjob@gmail.com>
2020-10-21 20:22:49 +0530

    Cross platform support. Includes suitable header files based on Target

--- include/kernel.h
+++ include/kernel.h
@@ -1,26 +1,19 @@
-/*
- * External Kernel routines, used by the kernel. These routines are not system
- * calls and may not be accessible by user mode programs.
- *
- * Note:
- * Remember that these header files are for building OS and its utilitites, it
- * is not a SDK.
- * */
 
 #ifndef __KERNEL_H__
 #define __KERNEL_H__
 
 #include <ints.h>
-/*
- * Displays a 16 bit number in Hexadecimal representation on the screen.
- */
-void kprinthex(u16 num);
 
-/*
- * Prints a ASCIZ string on the VGA text mode frame buffer.
- */
-void prints(char *s);
+#ifdef __linux__
+//#error "You are targetting Linux. Which is wrong!"
+#endif 
 
-void gdt_init();
-void gdt_segment_add(u8 gdt_index, u32 base, u32 limit, u8 access, u8 flags);
-#endif //__KERNEL_H__
+#ifndef __i386__
+#error "You are not targetting i386."
+#endif
+
+#ifdef __i386__
+    #include <x86/kernel.h>
+#endif
+
+#endif
