41d4bfbdef1c75b510cb8bb4648a8a8845c16e8e
coderarjob <mukherjeearjob@gmail.com>
2020-11-01 17:13:03 +0530

    Testing out the VGA driver and printk functions

--- kernel/x86/kernel.c
+++ kernel/x86/kernel.c
@@ -12,9 +12,30 @@
 */
 
 #include <kernel.h>
+#include <stdarg.h>
 
-__attribute__((noreturn)) 
+void kpanic(const char *s,...);
+
+//__attribute__((noreturn)) 
 void __main()
 {
+    kdisp_init();
+    printk(PK_ONSCREEN,"\r\nKernel starting..");
+
+    u8 attr;
+    kdisp_ioctl(DISP_GETATTR, &attr);
+    printk(PK_ONSCREEN,"\n\rAttr = %x", attr);
+
+    kpanic("Invalid error: %s:%x.",__FILE__, __LINE__);
     while(1);
+}
+
+void kpanic(const char *s,...)
+{
+    va_list l;
+    va_start(l,s);
+    kdisp_ioctl(DISP_SETCOORDS,0,0);
+    kdisp_ioctl(DISP_SETATTR, RED);
+    vprintk(s,l);
+    va_end(l);
 }
