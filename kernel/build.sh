e64847e4207c7694481eb834282b2a7268299216
coderarjob <mukherjeearjob@gmail.com>
2020-11-11 00:01:55 +0530

    kernel.ld links x86_kernel.o first then *.o ; kernel/build.sh calls ./x86/build.sh and links all *.o files in one go

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 
-# Builds Kernel
-# The procedures here are for the x86 architecture. Later on if I see that for
-# all the other supported architecture, minor changes are required, then this
-# same script will build all the others. 
+# Builds Kernel (Platform dependent and independent parts)
+export K_OBJDIR="$OBJDIR/kernel"
+
+#build path
+if [ -e $K_OBJDIR ]; then
+    rm $K_OBJDIR/* || exit
+else
+    mkdir $K_OBJDIR || exit
+fi
+
+# Build the Platform dependent Kernel
+bash kernel/x86/build.sh || exit
 
 # Build the Kernel
-echo "    [ Compilling Common Kernel ]    "
-#nasm -f elf32 kernel/x86/gdt_write.s \
-#    -I kernel/x86 $NASM_INCPATH \
-#    -O0 -o $OBJDIR/gdt_write.o \
-#    -l $LISTDIR/gdt_write.lst || exit
+echo "    [ Compiling Kernel: Platform independent parts ]    "
 
-i686_GCC="$GCC32" 
+$GCC32 -c kernel/printk.c -o $K_OBJDIR/printk.o  || exit
+$GCC32 -S kernel/printk.c -o $LISTDIR/printk.lst || exit
 
-$i686_GCC -c kernel/printk.c -o $OBJDIR/printk.o  || exit
-$i686_GCC -S kernel/printk.c -o $LISTDIR/printk.lst || exit
+$GCC32 -c kernel/mem.c -o $K_OBJDIR/mem.o  || exit
+$GCC32 -S kernel/mem.c -o $LISTDIR/mem.lst || exit
 
-$i686_GCC -c kernel/mem.c -o $OBJDIR/mem.o  || exit
-$i686_GCC -S kernel/mem.c -o $LISTDIR/mem.lst || exit
+$GCC32 -c kernel/kpanic.c -o $K_OBJDIR/kpanic.o  || exit
+$GCC32 -S kernel/kpanic.c -o $LISTDIR/kpanic.lst || exit
 
-$i686_GCC -c kernel/kpanic.c -o $OBJDIR/kpanic.o  || exit
-$i686_GCC -S kernel/kpanic.c -o $LISTDIR/kpanic.lst || exit
+$GCC32 -c kernel/errno.c -o $K_OBJDIR/errno.o  || exit
+$GCC32 -S kernel/errno.c -o $LISTDIR/errno.lst || exit
 
-$i686_GCC -c kernel/errno.c -o $OBJDIR/errno.o  || exit
-$i686_GCC -S kernel/errno.c -o $LISTDIR/errno.lst || exit
-
-$LD_KERNEL $OBJDIR/kernel_x86.o \
-           $OBJDIR/printk.o \
-           $OBJDIR/mem.o \
-           $OBJDIR/kpanic.o \
-           $OBJDIR/errno.o \
-           -o $OBJDIR/kernel.elf
-
-$OBJCOPY -O binary $OBJDIR/kernel.elf $OBJDIR/kernel.flt
+$LD_KERNEL $K_OBJDIR/*.o -o $K_OBJDIR/kernel.elf || exit
+$OBJCOPY -O binary $K_OBJDIR/kernel.elf $OBJDIR/kernel.flt || exit
