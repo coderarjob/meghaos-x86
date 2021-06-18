b07900259d48592c9f659d06748fa3b6d7d16763
coderarjob <mukherjeearjob@gmail.com>
2021-06-19 00:18:56 +0530

    printk know recognises type modifiers in format string: %ll and %l.

--- src/kernel/printk.c
+++ src/kernel/printk.c
@@ -14,32 +14,32 @@
 #include <kernel.h>
 #include <stdarg.h>
 
-// I could not simplly print to port E9 everytime printf is called. That way the messages will always be printed
-// on screen but only displayed to console if DEBUG is set. Only way to disable printing to screen is to delete
-// the printk statements.
-// Because of the above problem, and to have better control which message is printed where, I have two separate
-// 'destinations' one vga and another debug.
-// Note that the kdisp_* and kdebug_* functions can have different implementations based on the target architecure
-// or debug environment. But the implementation of vprintk and printk should not change.
-struct kconsole_operations{
-    void (*ioctl)(u8 request, ...);
-    void (*init)();
+enum _type_modifiers {
+    INT, LONG, LONG_LONG
+}; 
+
+enum _processing_states{
+    FLAGS,WIDTH,PRECISION,TYPE,CONVERTION,LITERAL,ERROR,COMPLETED
+}; 
+
+// I could not simplly print to port E9 everytime printf is called. That way 
+// the messages will always be printed on screen but only displayed to console 
+// if DEBUG is set. Only way to disable printing to screen is to delete the 
+// printk statements.  Because of the above problem, and to have better 
+// control which message is printed where, I have two separate 'destinations' 
+// one vga and another debug.  Note that the kdisp_* and kdebug_* functions 
+// can have different implementations based on the target architecure or debug 
+// environment. But the implementation of vprintk and printk should not change.
+static struct kconsole_operations{
     void (*putc)(char c);
+} conop[] = {
+    { .putc = kdisp_putc },
+    { .putc = kdebug_putc }
 };
 
-struct kconsole_operations conop[] = {
-    {
-        .ioctl = kdisp_ioctl,
-        .init  = kdisp_init,
-        .putc = kdisp_putc
-    },
-    {
-        .putc = kdebug_putc
-    },
-};
-
-static void pnum_base(struct kconsole_operations*, u32 num, u32 base);
+static void pnum_base(struct kconsole_operations*, u64 num, u32 base);
 static void pstring(struct kconsole_operations*, const char *s);
+static u64 _get_int_arg(va_list *list, enum _type_modifiers type_modifier);
 
 void printk(u8 type, const char *fmt, ...)
 {
@@ -54,62 +54,127 @@ void printk(u8 type, const char *fmt, ...)
     va_end(list);
 }
 
+static enum _processing_states 
+_convertion_processing(struct kconsole_operations *cop, 
+                              enum _type_modifiers type_mod,
+                              const char *template, va_list *list)
+{
+    u64 d;
+    char *s;
+
+    switch(*template){
+        case 'x':
+            d = _get_int_arg(list,type_mod);
+            pnum_base(cop,d,16);
+            break;
+        case 'd':
+            d = _get_int_arg(list,type_mod);
+            pnum_base(cop,d,10);
+            break;
+        case 'o':
+            d = _get_int_arg(list,type_mod);
+            pnum_base(cop,d,8);
+            break;
+        case 'b':
+            d = _get_int_arg(list,type_mod);
+            pnum_base(cop,d,2);
+            break;
+        case 's':
+            s = va_arg(*list,char *);
+            pstring(cop,s);
+            break;
+        case '%':
+            cop->putc('%');
+            break;
+        default:
+            return ERROR;
+            break;
+    } 
+
+    return COMPLETED;
+}
+
+static enum _processing_states 
+_type_processing(const char *template, enum _type_modifiers *tmod)
+{
+    switch(*template){
+        case '%':
+            break;
+        case 'l':
+            *tmod = (*tmod == LONG) ? LONG_LONG: LONG;
+            break;
+        default:
+            return CONVERTION;
+    }
+
+    return TYPE;
+}
+
 /* Prints formatted on screen at the cursor location.*/
 void vprintk(u8 type, const char *fmt, va_list list)
 {
+    enum _type_modifiers type_mod;
+    enum _processing_states state;
     struct kconsole_operations *cop = &conop[type];
 
-    int d;                   // Compiler always infers numeric literial as int,
-                             // until 'L', 'LL', etc override is provided.
-    const char *s;
-    for (const char *c = fmt;*c ; c++)
-    {
-        if (*c == '%') {
-            switch (*++c) {
-                case '%':
-                    cop->putc('%');
+    for (const char *c = fmt ; *c; c++) {
+        type_mod = INT;
+        state = (*c == '%') ? TYPE : LITERAL;
+
+        const char *cs = c;
+        while(*cs && state != COMPLETED && state != LITERAL) {
+            switch(state){
+                case FLAGS:
                     break;
-                case 'x':
-                    d = va_arg(list,int);
-                    pnum_base(cop,d,16);
+                case WIDTH:
                     break;
-                case 'd':
-                    d = va_arg(list,int);
-                    pnum_base(cop,d,10);
+                case PRECISION:
                     break;
-                case 'o':
-                    d = va_arg(list,int);
-                    pnum_base(cop,d,8);
+                case TYPE:
+                    if ((state = _type_processing(cs, &type_mod)) == TYPE)
+                        cs++;
                     break;
-                case 'b':
-                    d = va_arg(list,int);
-                    pnum_base(cop,d,2);
-                    break;
-                case 's':
-                    s = va_arg(list,char *);
-                    pstring(cop,s);
+                case CONVERTION:
+                    state = _convertion_processing(cop, type_mod, cs, &list);
+                    if (state == ERROR)
+                        state = LITERAL;
+                    else
+                        c = cs;
                     break;
                 default:
-                    // No need to show any error.
-                    // No need to panic either.
                     break;
             }
-            continue;
         }
 
-        cop->putc(*c);
+        if (state == LITERAL)
+            cop->putc(*c);
     }
 }
 
-/* Displays a 32 bit number in various representation on the screen.  */
-static void pnum_base(struct kconsole_operations *cop, 
-                      u32 num, u32 base)
+static u64 _get_int_arg(va_list *list, enum _type_modifiers type_modifier)
+{
+    switch (type_modifier)
+    {
+        case LONG:
+            return va_arg(*list,long int);
+            break;
+        case LONG_LONG:
+            return va_arg(*list,long long int);
+            break;
+        case INT:
+            return va_arg(*list,int);
+            break;
+    }
+}
+
+/* Displays a 64 bit number in various representation on the screen.  */
+static void pnum_base(struct kconsole_operations *cop, u64 num, u32 base) 
 {
     char *chars = "0123456789ABCDEF";
-    
+
     // Holds space for the largest possible representation. 
-    // Example: 33 characters in case of 32 bit int. (Base 2)
-    char output[33] = {0};       
+    // Example: 65 characters in case of 64 bit int. (Base 2)
+    char output[65] = {0};       
     int i = 0;
     do{
         output[i++] = chars[num % base];
