dd647af0fb172560cdf018422dcb9b2262c71ece
coderarjob <mukherjeearjob@gmail.com>
2021-09-07 23:43:50 +0530

    limits.h has valid ranges for kernel attributes.
    * include/limits.h and platform dependent include/x86/limits.h header
      files contain various MAX, MIN values used in kernel.

---
+++ include/limits.h
@@ -0,0 +1,20 @@
+/*
+* ---------------------------------------------------------------------------
+* Megha Operating System V2 - Cross Platform Kernel - Limits
+*
+* Note:
+* Remember that these header files are for building OS and its utilitites, it
+* is not a SDK.
+* ---------------------------------------------------------------------------
+*
+* Dated: 7th September 2021
+*/
+
+#ifndef __LIMITS_H__
+#define __LIMITS_H__
+
+#ifdef __i386__
+    #include <x86/limits.h>
+#endif
+
+#endif //__LIMITS_H__
---
+++ include/x86/limits.h
@@ -0,0 +1,27 @@
+/*
+* ---------------------------------------------------------------------------
+* Megha Operating System V2 - x86 Kernel - Limits
+*
+* Note:
+* Remember that these header files are for building OS and its utilitites, it
+* is not a SDK.
+* ---------------------------------------------------------------------------
+*
+* Dated: 7th September 2021
+*/
+
+#ifndef __LIMITS_H_X86__
+#define __LIMITS_H_X86__
+    
+    #define MAX_VGA_COLUMNS                 80
+    #define MAX_VGA_ROWS                    50
+    
+    #define MAX_PRINTABLE_STRING_LENGTH     MAX_VGA_COLUMNS * MAX_VGA_ROWS
+    
+    // Minimum index that can be editted in Kernel.
+    #define GDT_MIN_INDEX                   3     
+
+    // Number of GDT entries in memory
+    #define GDT_MAX_COUNT                   512   
+
+#endif // __LIMITS_H_X86__
