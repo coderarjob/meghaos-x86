75f4a97cda035c8b651f1e9b58fa84af7ef13369
Arjob Mukherjee <mukherjeearjob@gmail.com>
2021-12-28 23:41:36 +0530

    New scripts to automate the codestyle changes.
    * changenames.sh - Enforce typedef, global function, variable and macro
                       names.
    * indent.sh      - Enforce indentation rules.

---
+++ tools/indent.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# --------------------------------------------------------------------------
+# Space before every (
+# Note however this rule, does not apply to macro definations. 
+# There should be no space between macro name and '('. This will be fixed 
+# later in the 'fix'.
+# ---------------------------------------------------------------------------
+E="\(\w\)\((\)/\1\ ("
+find -type f \( -name "*.c" -o -name "*.h" \) \
+     -exec sed -i "s/$E/g" {} \;
+
+     # ---- Appy Fix for Macros
+     # There should be no space between macro name and '('. 
+     # '#define macro (..)' fixed to 'define macro(..)'
+
+    E="\(\W*#\W*define\W\+\w\+\)\ (/\1("
+    find -type f \( -name "*.c" -o -name "*.h" \) \
+         -exec sed -i "s/$E/g" {} \;
+    # ----
+
+# --------------------------------------------------------------------------
+# Space around binary and ternary operators. 
+# `=  +  -  <  > * /  %  |  &  ^  <=  >=  ==  !=  ?  :`
+# ---------------------------------------------------------------------------
+
+# Operators `=  +  -  <  >  %  |  &  ^  <=  >=  ==  !=  ?  :`
+AR=("= + - < > % | & ^ <= >= == != ? :")
+for op in ${AR[@]}; do
+    E="\(\w\)\($op\)\(\w\)/\1\ \2\ \3"
+    find -type f \( -name "*.c" -o -name "*.h" \) \
+         -exec sed -i "s/${E}/g" {} \;
+done
+
+# Operators `*`
+E="\(\w\)\(*\)\(\w\)/\1\ \2\ \3"
+find -type f \( -name "*.c" -o -name "*.h" \) \
+     -exec sed -i "s/$E/g" {} \;
+
+    # ---- Appy Fix for (%x, %llx -> % x, % llx)
+    E="\(%\ \)\(x\|llx\)/%\2"
+    find -type f \( -name "*.c" -o -name "*.h" \) \
+         -exec sed -i "s/$E/g" {} \;
+    # ----
