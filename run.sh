44c64277862084cb85a876f70e512a00cef06290
Arjob Mukherjee <mukherjeearjob@gmail.com>
2022-01-03 22:22:57 +0530

    run.sh now can pass startup arguments to qemu.

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-qemu-system-i386 -fda ./build/diskimage/x86/mos.flp \
-                 -boot a                            \
-                 -m 05                              \
-                 -cpu 486                           \
-                 -debugcon stdio                    \
-                 -no-reboot                         \
-                 -no-shutdown                       \
-                 -d cpu_reset
+OPTS="$@"
+RAMMB=5
+qemu-system-i386 $OPTS -fda ./build/diskimage/x86/mos.flp \
+                         -boot a                          \
+                         -m $RAMMB                        \
+                         -cpu 486                         \
+                         -debugcon stdio                  \
+                         -no-reboot                       \
+                         -no-shutdown                     \
+                         -d cpu_reset
                  
