4d8480c141c09c0fdcf3b148151baf672d6eda1c
Arjob Mukherjee <mukherjeearjob@gmail.com>
2022-06-27 03:49:49 +0530

    Unittest of kpmm_alloc added.
    * Mock files updated for this test to compile.

--- include/mock/kernel/x86/boot.h
+++ include/mock/kernel/x86/boot.h
@@ -8,6 +8,20 @@
 DECLARE_FUNC_0 (BootLoaderInfo *, kboot_getCurrentBootLoaderInfo);
 DECLARE_FUNC_1 (U16, kboot_getBootLoaderInfoFilesCount, IN, BootLoaderInfo const*);
 DECLARE_FUNC_1 (ULLONG, kboot_calculateAvailableMemory, IN, BootLoaderInfo const*);
+DECLARE_FUNC_2 (BootFileItem*
+               , kboot_getBootLoaderInfoBootFileItem
+               , IN, BootLoaderInfo const*
+               , IN, INT);
+DECLARE_FUNC_2 (BootMemoryMapItem*
+               , kboot_getBootLoaderInfoBootMemoryMapItem
+               , IN, BootLoaderInfo const*
+               , IN, INT);
+DECLARE_FUNC_1 (U16, kboot_getBootLoaderInfoBootMemoryMapItemCount, IN, BootLoaderInfo const*);
+DECLARE_FUNC_1 (U16, kboot_getBootFileItemFileLength, IN, BootFileItem const*);
+DECLARE_FUNC_1 (U32, kboot_getBootFileItemStartLocation, IN, BootFileItem const*);
+DECLARE_FUNC_1 (BootMemoryMapTypes, kboot_getBootMemoryMapItemType, IN, BootMemoryMapItem const*);
+DECLARE_FUNC_1 (U64, kboot_getBootMemoryMapItemLengthBytes, IN, BootMemoryMapItem const*);
+DECLARE_FUNC_1 (U64, kboot_getBootMemoryMapItemBaseAddress, IN, BootMemoryMapItem const*);
 
 void resetBootFake();
 #endif //BOOT_FAKE_X86_H
--- src/unittests/x86/pmm_test.c
+++ src/unittests/x86/pmm_test.c
@@ -3,15 +3,31 @@
 #include <mock/kernel/mem.h>
 #include <mock/kernel/x86/boot.h>
 
-void* CAST_PA_TO_VA (PHYSICAL a);
-
-PHYSICAL g_pab;
+static U8 pab[PAB_SIZE_BYTES];
 KernelErrorCodes k_errorNumber;
+PHYSICAL g_pab = PHYSICAL (pab);
 
-static U8 pab[PAB_SIZE_BYTES] = {0xFF};
+/* Fake Defination. At present, meghatest does not support varidac parameters. */
+void k_panic_ndu (const CHAR *s,...) { }
+void kdebug_printf_ndu (const CHAR *fmt, ...) { }
 
-TEST(PMM, init)
+TEST(PMM, alloc_fixed)
 {
+    // Will just set pointer `s_pab` to point to `pab` buffer defined here. No allocation or
+    // deallocation will take place as memory map count and files count are not set and will be at
+    // default zero.
+    kpmm_init();
+
+    // Allocating 1 byte means nothing to PMM. It should allocate 1 full page.
+    USYSINT bytesCount = 1;
+    PHYSICAL startAddress = PHYSICAL (0);
+
+    bool success = kpmm_alloc (NULL, &bytesCount, PMM_FIXED, startAddress);
+
+    EQ_SCALAR (bytesCount, CONFIG_PAGE_FRAME_SIZE_BYTES);
+    EQ_SCALAR (pab[0], 0x1);
+    EQ_SCALAR (pab[1], 0x0);
+    EQ_SCALAR (success, true);
     END();
 }
 
@@ -21,11 +37,8 @@ void reset()
     resetBootFake();
 }
 
-/* Fake Defination. At present, meghatest does not support varidac parameters. */
-void k_panic_ndu (const CHAR *s,...) { }
-void kdebug_printf_ndu (const CHAR *fmt, ...) { }
 
 int main()
 {
-    init();
+    alloc_fixed();
 }
