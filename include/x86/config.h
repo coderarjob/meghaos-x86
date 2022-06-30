a3fe1307aae3e34cf19c16fc1887311cb1a10eb7
Arjob Mukherjee <mukherjeearjob@gmail.com>
2022-07-01 03:24:28 +0530

    BugFix: Math error due to lack of parentheses

--- include/x86/config.h
+++ include/x86/config.h
@@ -9,12 +9,12 @@
 
 #ifndef MOS_CONFIG_X86_H
 #define MOS_CONFIG_X86_H
-    #define KB 1024
-    #define MB 1024 * KB
-    #define GB 1024 * MB
+    #define KB (1024)
+    #define MB (1024 * KB)
+    #define GB (1024 * MB)
 
-    #define CONFIG_PAGE_FRAME_SIZE_BYTES    4 * KB
-    #define CONFIG_MIN_RAM_BYTES            2 * MB
+    #define CONFIG_PAGE_FRAME_SIZE_BYTES    (4 * KB)
+    #define CONFIG_MIN_RAM_BYTES            (2 * MB)
     #define CONFIG_VGA_DISPLAY              "80x50"
 
 #endif // MOS_CONFIG_X86_H
