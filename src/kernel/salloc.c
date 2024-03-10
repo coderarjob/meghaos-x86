e8e25565769010838fd0f49d7d9097842626c69a
Arjob Mukherjee <mukherjeearjob@gmail.com>
2024-03-11 02:16:51 +0530

    Minor refactoring, using Utils macros instead

--- src/kernel/salloc.c
+++ src/kernel/salloc.c
@@ -22,9 +22,9 @@
     #include <x86/memloc.h>
 #endif
 
-#define SALLOC_MEM_END (SALLOC_MEM_START + (SALLOC_MEM_SIZE_PAGES * CONFIG_PAGE_FRAME_SIZE_BYTES))
+#define SALLOC_MEM_END (SALLOC_MEM_START + PAGEFRAMES_TO_BYTES (SALLOC_MEM_SIZE_PAGES))
 
-void *next = NULL; // Points to the start of next allocation.
+void* next = NULL; // Points to the start of next allocation.
 
 void salloc_init()
 {
@@ -41,8 +41,8 @@ void salloc_init()
      * required because there is no other way to reserve virtual pages */
     for (UINT pageIndex = 0; pageIndex < SALLOC_MEM_SIZE_PAGES; pageIndex++)
     {
-        PTR this_va = SALLOC_MEM_START + (pageIndex * CONFIG_PAGE_FRAME_SIZE_BYTES);
-        Physical this_pa = PHYSICAL(pa.val + (pageIndex * CONFIG_PAGE_FRAME_SIZE_BYTES));
+        PTR      this_va = SALLOC_MEM_START + PAGEFRAMES_TO_BYTES (pageIndex);
+        Physical this_pa = PHYSICAL (pa.val + PAGEFRAMES_TO_BYTES (pageIndex));
         if (kpg_map (pd, this_va, this_pa, PG_MAP_FLAG_KERNEL) == false)
             k_panic ("Page map failed");
     }
@@ -52,29 +52,28 @@ void salloc_init()
 
 // TODO: salloc should take flags for alignment requirements to meet various placement requirements.
 // For example: Page boundary must not ocour within TSS.
-void *salloc (UINT bytes)
+void* salloc (UINT bytes)
 {
-    FUNC_ENTRY("Bytes: 0x%px", bytes);
+    FUNC_ENTRY ("Bytes: 0x%px", bytes);
 
     UINT allocSize = ALIGN_UP (bytes, SALLOC_GRANUALITY);
-    INFO("Size after aligning: 0x%px", allocSize);
+    INFO ("Size after aligning: 0x%px", allocSize);
 
     if ((PTR)next >= SALLOC_MEM_END)
     {
         RETURN_ERROR (ERR_OUT_OF_MEM, NULL);
     }
 
-    next = (void *)((PTR)next + allocSize);
-    return (void *)((PTR)next - allocSize);
-
+    next = (void*)((PTR)next + allocSize);
+    return (void*)((PTR)next - allocSize);
 }
 
-void *scalloc (UINT bytes)
+void* scalloc (UINT bytes)
 {
-    FUNC_ENTRY("Bytes: 0x%px", bytes);
+    FUNC_ENTRY ("Bytes: 0x%px", bytes);
 
-    void *mem = salloc (bytes);
+    void* mem = salloc (bytes);
     if (mem != NULL)
-        k_memset(mem, 0, bytes);
+        k_memset (mem, 0, bytes);
     return mem;
 }
