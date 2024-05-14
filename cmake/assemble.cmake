72f8813163f47e6c4c4759e0dc36f90a0e24360f
Arjob Mukherjee <mukherjeearjob@gmail.com>
2024-05-14 21:46:17 +0530

    cmake: Not using inbuild ASM_NASM module
    The inbuild added complexity and I find using custom commands and
    targets to be useful and better control.

--- cmake/assemble.cmake
+++ cmake/assemble.cmake
@@ -1,5 +1,10 @@
+find_program(NASM NAMES nasm)
+if (NOT NASM)
+    message(FATAL_ERROR "nasm assembler required and was not found.")
+endif()
+
 function(assemble)
-    set(oneValueArgs TARGET SOURCE COPYTO LISTDIR)
+    set(oneValueArgs TARGET SOURCE OUTFILE LISTDIR)
     set(multiValueArgs FLAGS INCLUDE_DIRECTORIES)
     set(options)
     cmake_parse_arguments(PARSE_ARGV 0 ASSEMBLE "${options}" "${oneValueArgs}" "${multiValueArgs}")
@@ -7,45 +12,39 @@ function(assemble)
     # -------------------------------------------------------------------------------------------
     # Check validity
     # -------------------------------------------------------------------------------------------
-    if (NOT ASSEMBLE_TARGET OR NOT ASSEMBLE_SOURCE OR NOT ASSEMBLE_FLAGS)
-        message(FATAL_ERROR "Target, Assembly source and flags must be given.")
-    endif()
-
-    if (NOT ASSEMBLE_COPYTO)
-        message(FATAL_ERROR "COPYTO must be provided.")
+    if (NOT ASSEMBLE_TARGET OR NOT ASSEMBLE_SOURCE OR NOT ASSEMBLE_OUTFILE)
+        message(FATAL_ERROR "Target, assembly source and output binary filename must be given.")
     endif()
 
     # -------------------------------------------------------------------------------------------
-    # Object target requirements
+    # Directories
     # -------------------------------------------------------------------------------------------
-    add_library(${ASSEMBLE_TARGET} OBJECT ${ASSEMBLE_SOURCE})
-    target_include_directories(${ASSEMBLE_TARGET} PUBLIC ${ASSEMBLE_INCLUDE_DIRECTORIES})
-
-    # -------------------------------------------------------------------------------------------
-    # Add assembler config to generate list files and cmake rule to copy the list file to
-    # the provided directory.
-    # -------------------------------------------------------------------------------------------
+    set(ASM_BIN_FILE ${ASSEMBLE_OUTFILE})
     if (ASSEMBLE_LISTDIR)
-        set(list_file "${ASSEMBLE_LISTDIR}/${ASSEMBLE_TARGET}.lst")
-        list(APPEND ASSEMBLE_FLAGS -l ${list_file})
-
-        add_custom_command(
-            OUTPUT ${list_file}
-            DEPENDS $<TARGET_OBJECTS:${ASSEMBLE_TARGET}>
-            COMMAND ${CMAKE_COMMAND} -E touch ${list_file}
-            )
-
+        get_filename_component(list_file_title ${ASSEMBLE_SOURCE} NAME_WE)
+        set(ASM_LIST_FILE "${ASSEMBLE_LISTDIR}/${list_file_title}.lst")
     endif()
 
-    target_compile_options(${ASSEMBLE_TARGET} PRIVATE ${ASSEMBLE_FLAGS})
+    # -------------------------------------------------------------------------------------------
+    # Gather include directories
+    # -------------------------------------------------------------------------------------------
+    foreach(DIR ${ASSEMBLE_INCLUDE_DIRECTORIES})
+        set(ASM_INC_DIR_FLAGS ${ASM_INC_DIR_FLAGS} -I ${DIR})
+    endforeach()
 
     # -------------------------------------------------------------------------------------------
-    # Copy to a destination directory
+    # Invoke nasm to build bin and list files
     # -------------------------------------------------------------------------------------------
     add_custom_command(
-        OUTPUT ${ASSEMBLE_COPYTO}
-        DEPENDS $<TARGET_OBJECTS:${ASSEMBLE_TARGET}>
-        COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_OBJECTS:${ASSEMBLE_TARGET}> ${ASSEMBLE_COPYTO}
+        OUTPUT ${ASM_BIN_FILE} ${ASM_LIST_FILE}
+        DEPENDS ${ASSEMBLE_SOURCE}
+        COMMAND ${NASM} ${ASSEMBLE_FLAGS} ${ASSEMBLE_SOURCE}
+                ${ASM_INC_DIR_FLAGS}
+                -o ${ASM_BIN_FILE} -l ${ASM_LIST_FILE}
+        COMMENT "Building ${ASSEMBLE_TARGET}"
         )
 
+    add_custom_target(${ASSEMBLE_TARGET} ALL
+        DEPENDS ${ASM_BIN_FILE} ${ASM_LIST_FILE}
+        )
 endfunction()
