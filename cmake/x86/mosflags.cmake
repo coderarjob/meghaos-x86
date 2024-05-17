170aff1e961758433db12ccb46fec2e79f6a152c
Arjob Mukherjee <mukherjeearjob@gmail.com>
2024-05-17 23:37:40 +0530

    cmake: build order fixed
    Also in this commit:
    * Build options and flags are moved form toolchain file to a arch
      specific separate cmake file.
    * Cache variables to setting up build mode and debug level.
    * Compiler build flags determined by the cmake cache variables.

---
+++ cmake/x86/mosflags.cmake
@@ -0,0 +1,49 @@
+set(MOS_GCC_WARN_FLAGS
+    -Wpedantic
+    -Wall
+    -Wextra
+    -Wconversion
+    -Wdangling-else
+    -Werror
+    )
+
+set(MOS_GCC_FLAGS
+    ${MOS_GCC_WARN_FLAGS}
+    -std=c99
+    -nostartfiles
+    -ffreestanding
+    -fno-pie
+    -fno-stack-protector
+    -fno-asynchronous-unwind-tables
+    -m32
+    -march=i386
+    -masm=intel
+    -mno-red-zone
+    -mno-sse
+    -O1
+    -fno-unit-at-a-time
+    -fno-omit-frame-pointer
+    -fno-inline-functions-called-once
+    -DDEBUG_LEVEL=${MOS_DEBUG_LEVEL}
+    -D${MOS_BUILD_MODE}
+    )
+
+if (MOS_BUILD_MODE STREQUAL "Debug")
+    set(MOS_GCC_FLAGS ${MOS_GCC_FLAGS} -g)
+endif()
+
+set(MOS_LINKER_OPTIONS
+    -ffreestanding
+    -nostdlib
+    )
+
+set(MOS_ASM_INCLUDE_DIRS
+    ${PROJECT_SOURCE_DIR}/include/asm
+    ${PROJECT_SOURCE_DIR}/include/x86/asm
+    )
+
+set(MOS_GCC_INCLUDE_DIRS
+    ${PROJECT_SOURCE_DIR}/include
+    )
+
+set(MOS_LINKER_SCRIPT_FILE ${PROJECT_SOURCE_DIR}/build/kernel.ld)
--- tools/toolchain-i686-elf-pc.cmake
+++ tools/toolchain-i686-elf-pc.cmake
@@ -4,53 +4,6 @@ set(CMAKE_SYSTEM_PROCESSOR i686)
 set(CROSSCOMPILE_PATH ~/.local/opt/i686-cross)
 set(ARCH x86)
 
-set(MOS_ASM_INCLUDE_DIRS
-    ${PROJECT_SOURCE_DIR}/include/asm
-    ${PROJECT_SOURCE_DIR}/include/x86/asm
-    )
-
-set(MOS_GCC_INCLUDE_DIRS
-    ${PROJECT_SOURCE_DIR}/include
-    )
-
-set(MOS_GCC_WARN_FLAGS
-    -Wpedantic
-    -Wall
-    -Wextra
-    -Wconversion
-    -Wdangling-else
-    -Werror
-    )
-
-set(MOS_GCC_FLAGS
-    ${MOS_GCC_WARN_FLAGS}
-    -std=c99
-    -g
-    -nostartfiles
-    -ffreestanding
-    -fno-pie
-    -fno-stack-protector
-    -fno-asynchronous-unwind-tables
-    -m32
-    -march=i386
-    -masm=intel
-    -mno-red-zone
-    -mno-sse
-    -O1
-    -fno-unit-at-a-time
-    -fno-omit-frame-pointer
-    -fno-inline-functions-called-once
-    -DDEBUG
-    -DDEBUG_LEVEL=1
-    )
-
-set(MOS_LINKER_SCRIPT_FILE ${PROJECT_SOURCE_DIR}/build/kernel.ld)
-
-set(MOS_LINKER_OPTIONS
-    -ffreestanding
-    -nostdlib
-    )
-
 set(CMAKE_C_COMPILER i686-elf-gcc)
 set(CMAKE_FIND_ROOT_PATH ${CROSSCOMPILE_PATH})
 
