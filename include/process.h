2e9e25cb42679c09f9eabfc350da72663ccf9d8f
Arjob Mukherjee <mukherjeearjob@gmail.com>
2024-07-10 00:41:28 +0530

    Fix: PROCESS_FLAGS_NONE is a shortcut not a flag

--- include/process.h
+++ include/process.h
@@ -17,13 +17,12 @@ typedef enum ProcessStates {
 
 typedef enum ProcessFlags {
     PROCESS_FLAGS_NONE           = 0,
-    PROCESS_FLAGS_KERNEL_PROCESS = (1 << 1),
-    PROCESS_FLAGS_THREAD         = (1 << 2),
+    PROCESS_FLAGS_KERNEL_PROCESS = (1 << 0),
+    PROCESS_FLAGS_THREAD         = (1 << 1),
 } ProcessFlags;
 
-__asm__(".equ PROCESS_FLAGS_NONE, 0;"
-        ".equ PROCESS_FLAGS_KERNEL_PROCESS, (1 << 1);"
-        ".equ PROCESS_FLAGS_THREAD, (1 << 2);");
+__asm__(".equ PROCESS_FLAGS_KERNEL_PROCESS, (1 << 0);"
+        ".equ PROCESS_FLAGS_THREAD,         (1 << 1);");
 
 typedef struct ProcessRegisterState ProcessRegisterState;
 
