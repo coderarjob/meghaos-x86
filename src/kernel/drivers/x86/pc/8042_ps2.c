b603f3944b3a55263fc51438fb320fd6d13b9860
Arjob Mukherjee <arjobmukherjee@gmail.com>
2024-09-07 17:28:34 +0530

    Fix: PS2 initialization causing GP Fault
    The order in which the initialization was done was wrong.

--- src/kernel/drivers/x86/pc/8042_ps2.c
+++ src/kernel/drivers/x86/pc/8042_ps2.c
@@ -5,46 +5,51 @@
  */
 
 #include <drivers/x86/pc/8042_ps2.h>
+#include <stdbool.h>
 #include <types.h>
 #include <kdebug.h>
 #include <x86/io.h>
 #include <kassert.h>
+#include <utils.h>
 
-#define PS2_DATA_PORT                    0x60
-#define PS2_CMD_PORT                     0x64
+#define DATA_PORT                                (0x60)
+#define CMD_PORT                                 (0x64)
 
-#define PS2_CMD_DISABLE_SECOND_PORT      0xA7
-#define PS2_CMD_ENABLE_SECOND_PORT       0xA8
+#define CMD_DISABLE_SECOND_PORT                  (0xA7)
+#define CMD_ENABLE_SECOND_PORT                   (0xA8)
+#define CMD_DISABLE_FIRST_PORT                   (0xAD)
+#define CMD_ENABLE_FIRST_PORT                    (0xAE)
+#define CMD_TEST_CONTROLLER                      (0xAA)
+#define CMD_READ_CONFIGURATION_BYTE              (0x20)
+#define CMD_WRITE_CONFIGURATION_BYTE             (0x60)
+#define CMD_TEST_FIRST_PORT                      (0xAB)
+#define CMD_TEST_SECOND_PORT                     (0xA9)
+#define CMD_RESET_PORT                           (0xFF)
+#define CMD_SEND_TO_SECOND_PORT                  (0xD4)
 
-#define PS2_CMD_DISABLE_FIRST_PORT       0xAD
-#define PS2_CMD_ENABLE_FIRST_PORT        0xAE
-#define PS2_CMD_TEST_CONTROLLER          0xAA
-#define PS2_CMD_READ_CONFIGURATION_BYTE  0x20
-#define PS2_CMD_WRITE_CONFIGURATION_BYTE 0x60
-#define PS2_CMD_TEST_FIRST_PORT          0xAB
-#define PS2_CMD_TEST_SECOND_PORT         0xA9
-#define PS2_CMD_RESET_PORT               0xFF
+#define RES_TEST_CONTROLLER_PASSED               (0x55)
+#define RES_TEST_CONTROTEST_FAILED               (0xFC)
+#define RES_TEST_PORT_PASSED                     (0x00)
 
-#define PS2_RES_TEST_CONTROLLER_PASSED   0x55
-#define PS2_RES_TEST_CONTROTEST_FAILED   0xFC
-#define PS2_RES_TEST_PORT_PASSED         0x00
-#define PS2_RES_RESET_SUCCESS_BYTE0      0xFA
-#define PS2_RES_RESET_SUCCESS_BYTE1      0xAA
+#define DEV_CMD_RESET                            (0xFF)
+#define DEV_CMD_SET_TO_DEFAULT                   (0xF6)
+#define DEV_CMD_IDENTIFY                         (0xF2)
+#define DEV_CMD_DISABLE_SCANNING                 (0xF5)
+#define DEV_CMD_ENABLE_SCANNING                  (0xF4)
 
-#define IS_READ_READY(status)            (((status)&0x1) == 1)
-#define IS_WRITE_READY(status)           (((status)&0x2) == 0)
+#define DEV_RES_ACK                              (0xFA)
+#define DEV_RES_BAT_COMPLETE                     (0xAA)
 
-#define RW_TIMEOUT_ITERATION             1000U
+#define IS_READ_READY(status)                    (((status)&0x1) == 1)
+#define IS_WRITE_READY(status)                   (((status)&0x2) == 0)
 
-// Enable interrupt, clock & translation for 1st PS2 port
-#define PS2_CONFIG_FIRST_PORT            (0x51)
+#define RW_TIMEOUT_ITERATION                     4000U
 
-// Enable Interrupt, clock for 2nd PS2 port
-#define PS2_CONFIG_SECOND_PORT           (0x22)
-
-#define IS_DEVICE_RESET_SUCCESS(a, b)                                                  \
-    ((((a) == PS2_RES_RESET_SUCCESS_BYTE0) && ((b) == PS2_RES_RESET_SUCCESS_BYTE1)) || \
-     (((a) == PS2_RES_RESET_SUCCESS_BYTE1) && ((b) == PS2_RES_RESET_SUCCESS_BYTE0)))
+#define CONFIG_FIRST_PORT_INTERRUPT_ENABLE_BIT   (0U)
+#define CONFIG_SECOND_PORT_INTERRUPT_ENABLE_BIT  (1U)
+#define CONFIG_FIRST_PORT_CLOCK_DISABLE_BIT      (4U)
+#define CONFIG_SECOND_PORT_CLOCK_DISABLE_BIT     (5U)
+#define CONFIG_FIRST_PORT_TRANSLATION_ENABLE_BIT (6U)
 
 bool ps2_port_1_available, ps2_port_2_available;
 
@@ -52,9 +57,9 @@ static UINT read_port (UINT port)
 {
     // Wait for data to be in
     UINT i = 0, status = 0;
-    inb (PS2_CMD_PORT, status);
+    inb (CMD_PORT, status);
     for (; !IS_READ_READY (status) && i < RW_TIMEOUT_ITERATION; i++) {
-        inb (PS2_CMD_PORT, status);
+        inb (CMD_PORT, status);
     }
     k_assert (i < RW_TIMEOUT_ITERATION, "PS2: Long wait");
 
@@ -67,88 +72,125 @@ static void write_port (UINT port, UINT data)
 {
     // Wait for last data to be out
     UINT i = 0, status = 0xFF;
-    inb (PS2_CMD_PORT, status);
+    inb (CMD_PORT, status);
     for (; !IS_WRITE_READY (status) && i < RW_TIMEOUT_ITERATION; i++) {
-        inb (PS2_CMD_PORT, status);
+        inb (CMD_PORT, status);
     }
     k_assert (i < RW_TIMEOUT_ITERATION, "PS2: Long wait");
 
     outb (port, data);
 }
 
+static void read_configuration_byte (char* s)
+{
+    write_port (CMD_PORT, CMD_READ_CONFIGURATION_BYTE);
+    UINT config = read_port (DATA_PORT);
+    kdebug_println ("%s: %x", s, config);
+}
+
 void ps2_init()
 {
     FUNC_ENTRY();
 
-    UINT result;
+    read_configuration_byte ("First");
 
-    // TODO: Check ACPI tables to see if PS/2 controller exists. For now I will continue with the
-    // assumption that it does at least writing to the ports will not cause a failure if it does not
-    // exists.
+    // Step 1: Disable both PS/2 ports
+    // --------------------------------------------------------
+    write_port (CMD_PORT, CMD_DISABLE_FIRST_PORT);
+    write_port (CMD_PORT, CMD_DISABLE_SECOND_PORT);
 
-    // Disable both PS/2 ports
-    write_port (PS2_CMD_PORT, PS2_CMD_DISABLE_FIRST_PORT);
-    write_port (PS2_CMD_PORT, PS2_CMD_DISABLE_SECOND_PORT);
-
-    // Flush the output (from device perspective) buffer
+    // Step 2: Flush the output (from device perspective) buffer
+    // --------------------------------------------------------
     U8 discard;
-    inb (PS2_DATA_PORT, discard);
+    inb (DATA_PORT, discard);
 
-    // Perform Controller self test
-    write_port (PS2_CMD_PORT, PS2_CMD_TEST_CONTROLLER);
-    result = read_port (PS2_DATA_PORT);
-    k_assert (result == PS2_RES_TEST_CONTROLLER_PASSED, "PS2: Self test failed");
+    // Step 3: Test PS2 controller
+    // --------------------------------------------------------
+    write_port (CMD_PORT, CMD_TEST_CONTROLLER);
+    UINT result = read_port (DATA_PORT);
+    k_assert (result == RES_TEST_CONTROLLER_PASSED, "PS2: Self test failed");
 
-    // Test PS/2 ports (not the devices) & determine which port is working
-    write_port (PS2_CMD_PORT, PS2_CMD_TEST_FIRST_PORT);
-    ps2_port_1_available = (read_port (PS2_DATA_PORT) == PS2_RES_TEST_PORT_PASSED);
+    // Step 4: Determine if there are two channels
+    // --------------------------------------------------------
+    ps2_port_1_available = ps2_port_2_available = true;
+    write_port (CMD_PORT, CMD_TEST_FIRST_PORT);
+    result               = read_port (DATA_PORT);
+    ps2_port_1_available = (result == RES_TEST_PORT_PASSED);
 
-    write_port (PS2_CMD_PORT, PS2_CMD_TEST_SECOND_PORT);
-    ps2_port_2_available = (read_port (PS2_DATA_PORT) == PS2_RES_TEST_PORT_PASSED);
+    write_port (CMD_PORT, CMD_TEST_SECOND_PORT);
+    result               = read_port (DATA_PORT);
+    ps2_port_2_available = (result == RES_TEST_PORT_PASSED);
 
     k_assert (ps2_port_1_available || ps2_port_2_available, "PS2: No ports working.");
 
-    // Configure PS2 and enable ports
-    write_port (PS2_CMD_PORT, PS2_CMD_READ_CONFIGURATION_BYTE);
-    UINT config = read_port (PS2_DATA_PORT);
-    if (ps2_port_1_available) {
-        write_port (PS2_CMD_PORT, PS2_CMD_ENABLE_FIRST_PORT);
-        config |= PS2_CONFIG_FIRST_PORT;
-    }
-    if (ps2_port_2_available) {
-        write_port (PS2_CMD_PORT, PS2_CMD_ENABLE_SECOND_PORT);
-        config |= PS2_CONFIG_SECOND_PORT;
-    }
-    write_port (PS2_CMD_PORT, PS2_CMD_WRITE_CONFIGURATION_BYTE);
-    write_port (PS2_DATA_PORT, config);
-    kdebug_println("Config written: %x", config);
+    // Step 5: Setup configuration byte
+    // --------------------------------------------------------
+    write_port (CMD_PORT, CMD_READ_CONFIGURATION_BYTE);
+    UINT config = read_port (DATA_PORT);
 
-    // Reset device (Port 1)
     if (ps2_port_1_available) {
-        write_port (PS2_DATA_PORT, PS2_CMD_RESET_PORT);
-        result = read_port (PS2_DATA_PORT); kdebug_println ("result (1st): %x", result);
-        if (result == PS2_RES_RESET_SUCCESS_BYTE0 || result == PS2_RES_RESET_SUCCESS_BYTE1) {
-            // On success first byte is either 0xFA or 0xAA. Got that
-            result = read_port (PS2_DATA_PORT); kdebug_println ("result (2st): %x", result);
-            if (result == PS2_RES_RESET_SUCCESS_BYTE0 || result == PS2_RES_RESET_SUCCESS_BYTE1) {
-                //result = read_port (PS2_DATA_PORT); kdebug_println ("result (3rd): %x", result);
-            }
-        }
+        config = BIT_SET (config, CONFIG_FIRST_PORT_INTERRUPT_ENABLE_BIT);
+        config = BIT_CLEAR (config, CONFIG_FIRST_PORT_TRANSLATION_ENABLE_BIT);
     }
 
-    if (ps2_port_2_available) {
-        write_port (PS2_CMD_PORT, 0xD4);
-        write_port (PS2_DATA_PORT, PS2_CMD_RESET_PORT);
-        result = read_port (PS2_DATA_PORT); kdebug_println ("result (1st): %x", result);
-        if (result == PS2_RES_RESET_SUCCESS_BYTE0 || result == PS2_RES_RESET_SUCCESS_BYTE1) {
-            // On success first byte is either 0xFA or 0xAA. Got that
-            result = read_port (PS2_DATA_PORT); kdebug_println ("result (2st): %x", result);
-            if (result == PS2_RES_RESET_SUCCESS_BYTE0 || result == PS2_RES_RESET_SUCCESS_BYTE1) {
-                //result = read_port (PS2_DATA_PORT); kdebug_println ("result (3rd): %x", result);
-            }
-        }
+    if (ps2_port_1_available) {
+        config = BIT_SET (config, CONFIG_SECOND_PORT_INTERRUPT_ENABLE_BIT);
     }
-    //write_port (PS2_CMD_PORT, PS2_CMD_READ_CONFIGURATION_BYTE);
-    //config = read_port (PS2_DATA_PORT);
-    //INFO("Config read 2: %x", config);
+
+    write_port (CMD_PORT, CMD_WRITE_CONFIGURATION_BYTE);
+    write_port (DATA_PORT, config);
+    kdebug_println ("Config written: %x", config);
+
+    read_configuration_byte ("step 4");
+
+    // Step 6: Enable both PS/2 ports
+    // --------------------------------------------------------
+    write_port (CMD_PORT, CMD_ENABLE_FIRST_PORT);  // This will turn on the clock for port 1
+    write_port (CMD_PORT, CMD_ENABLE_SECOND_PORT); // This will turn on the clock for port 2
+
+    read_configuration_byte ("step 5");
+
+    // Keyboard setup
+    if (ps2_port_1_available) {
+        // Identify device
+        write_port (DATA_PORT, DEV_CMD_DISABLE_SCANNING);
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+
+        write_port (DATA_PORT, DEV_CMD_IDENTIFY);
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+        UINT a = read_port (DATA_PORT), b = read_port (DATA_PORT);
+        kdebug_println ("Dev identity: %x %x", a, b);
+
+        write_port (DATA_PORT, DEV_CMD_SET_TO_DEFAULT); // Set defaults
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+
+        write_port (DATA_PORT, DEV_CMD_ENABLE_SCANNING); // Enable scanning
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+    }
+
+    // Mouse setup
+    if (ps2_port_1_available) {
+        // Identify device
+        write_port (CMD_PORT, CMD_SEND_TO_SECOND_PORT);
+        write_port (DATA_PORT, DEV_CMD_DISABLE_SCANNING);
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+
+        write_port (CMD_PORT, CMD_SEND_TO_SECOND_PORT);
+        write_port (DATA_PORT, DEV_CMD_IDENTIFY);
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+        UINT a, b = 0;
+        a = read_port (DATA_PORT);
+        //b = read_port (DATA_PORT);
+        kdebug_println ("Dev identity: %x %x", a, b);
+
+        write_port (CMD_PORT, CMD_SEND_TO_SECOND_PORT);
+        write_port (DATA_PORT, DEV_CMD_SET_TO_DEFAULT); // Set defaults
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+
+        write_port (CMD_PORT, CMD_SEND_TO_SECOND_PORT);
+        write_port (DATA_PORT, DEV_CMD_ENABLE_SCANNING); // Enable scanning
+        k_assert (read_port (DATA_PORT) == DEV_RES_ACK, "ACK not received");
+    }
+
+    read_configuration_byte ("final");
 }
