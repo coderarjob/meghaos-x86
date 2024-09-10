fcb278e950dc3531f0db66ed0e6894b16ef24322
Arjob Mukherjee <arjobmukherjee@gmail.com>
2024-09-11 03:21:23 +0530

    PIT: Multiple refactoring changes
    * pit_stop_start_counter function is removed as its more of a Speaker
      function than a timer one.

--- include/drivers/x86/pc/8254_pit.h
+++ include/drivers/x86/pc/8254_pit.h
@@ -7,9 +7,28 @@
 #pragma once
 
 #include <types.h>
-#define X86_PIT_COUNTER_MODE2 (2U)
-#define X86_PIT_COUNTER_MODE3 (3U)
 
-void pit_get_interrupt_counter (U8* mode, U16* value);
-void pit_set_interrupt_counter (U8 mode, U16 value);
-void pit_set_speaker_counter (U16 value);
+typedef enum PITCounterModes {
+    PIT_COUNTER_MODE_2 = 2,
+    PIT_COUNTER_MODE_3 = 3
+} PITCounterModes;
+
+typedef enum X86_PITCounters {
+    PIT_COUNTER_0 = 0,
+    PIT_COUNTER_2 = 2
+} PITCounters;
+
+void pit_get_counter (PITCounters cntr, U8* status, U16* value);
+void pit_set_counter (PITCounters cntr, PITCounterModes mode, U16 value);
+void pit_get_interrupt_counter (PITCounterModes* mode, U16* value);
+void pit_stop_start_counter (PITCounters cntr, bool turnOn);
+
+static inline void pit_set_interrupt_counter (PITCounterModes mode, U16 value)
+{
+    pit_set_counter (PIT_COUNTER_0, mode, value);
+}
+
+static inline void pit_set_speaker_counter (U16 value)
+{
+    pit_set_counter (PIT_COUNTER_2, PIT_COUNTER_MODE_3, value);
+}
--- src/kernel/drivers/x86/pc/8254_pit.c
+++ src/kernel/drivers/x86/pc/8254_pit.c
@@ -20,9 +20,6 @@
 #define COUNTER2_PORT                0x42
 #define CONTROLWORD_PORT             0x43
 
-#define COUNTER0                     (0U)
-#define COUNTER2                     (2U)
-
 #define CONTROLWORD_RW_TWO_BYTES     (3U)
 
 #define CONTROLWORD(cntr, rw, mode)  (((cntr) << 6U) | ((rw) << 4U) | ((mode) << 1U))
@@ -31,12 +28,14 @@
 #define READBACK(cntr)               ((CONTROLWORD_READBACK_COMMAND << 6U) | (1 << ((cntr + 1))))
 #define READBACK_STATUS_MODE_MASK    (0xEU)
 
-static void set_counter (UINT cntr, UINT mode, U16 value)
+void pit_set_counter (PITCounters cntr, PITCounterModes mode, U16 value)
 {
-    k_assert (cntr == COUNTER0 || cntr == COUNTER2, "Invalid counter");
-    k_assert (mode == X86_PIT_COUNTER_MODE2 || mode == X86_PIT_COUNTER_MODE3, "Invalid counter");
+    FUNC_ENTRY ("counter: %x, mode: %x, value: %x", cntr, mode, value);
 
-    UINT port  = (cntr == COUNTER0) ? COUNTER0_PORT : COUNTER2_PORT;
+    k_assert (cntr == PIT_COUNTER_0 || cntr == PIT_COUNTER_2, "Invalid counter");
+    k_assert (mode == PIT_COUNTER_MODE_2 || mode == PIT_COUNTER_MODE_3, "Invalid counter");
+
+    UINT port  = (cntr == PIT_COUNTER_0) ? COUNTER0_PORT : COUNTER2_PORT;
     U8 byteLow = (value & 0xFFU), byteHigh = ((value >> 8U) & 0xFFU);
 
     // Write control word
@@ -45,15 +44,15 @@ static void set_counter (UINT cntr, UINT mode, U16 value)
     // Write initial count
     outb (port, byteLow);
     outb (port, byteHigh);
-
-    // INFO ("CWord: %x, port: %x, bytehigh: %x, bytelow: %x", cw, port, byteHigh, byteLow);
 }
 
-static void get_counter (UINT cntr, U8* status, U16* value)
+void pit_get_counter (PITCounters cntr, U8* status, U16* value)
 {
+    FUNC_ENTRY ("counter: %x, status: %px, value: %px", cntr, status, value);
+
     k_assert (status != NULL && value != NULL, "Invalid input");
-    k_assert (cntr == COUNTER0 || cntr == COUNTER2, "Invalid counter");
-    UINT port = (cntr == COUNTER0) ? COUNTER0_PORT : COUNTER2_PORT;
+    k_assert (cntr == PIT_COUNTER_0 || cntr == PIT_COUNTER_2, "Invalid counter");
+    UINT port = (cntr == PIT_COUNTER_0) ? COUNTER0_PORT : COUNTER2_PORT;
 
     //// Send controlword readback command
     UINT cw = READBACK (cntr);
@@ -66,34 +65,13 @@ static void get_counter (UINT cntr, U8* status, U16* value)
     inb (port, byteHigh);
 
     *value = (U16)((byteHigh << 8U) | byteLow);
-
-    // INFO ("CWord: %x, port: %x, status: %x, high: %x, low: %x", cw, port, *status, byteHigh,
-    // byteLow);
 }
 
-void pit_get_interrupt_counter (U8* mode, U16* value)
+void pit_get_interrupt_counter (PITCounterModes* mode, U16* value)
 {
     FUNC_ENTRY ("mode:  %px, value: %px", mode, value);
 
     U8 status;
-    get_counter (COUNTER0, &status, value);
+    pit_get_counter (PIT_COUNTER_0, &status, value);
     *mode = (status & READBACK_STATUS_MODE_MASK) >> 1;
-}
-
-void pit_set_interrupt_counter (U8 mode, U16 value)
-{
-    FUNC_ENTRY ("mode:  %x, value: %x", mode, value);
-    set_counter (COUNTER0, mode, value);
-}
-
-void pit_set_speaker_counter (U16 value)
-{
-    FUNC_ENTRY ("value: %x", value);
-    set_counter (COUNTER2, X86_PIT_COUNTER_MODE3, value);
-}
-
-void pit_stop_start_counter (U8 cntr, bool doStop)
-{
-    FUNC_ENTRY ("counter: %x, doStop:  %u", cntr, doStop);
-    UNREACHABLE();
 }
