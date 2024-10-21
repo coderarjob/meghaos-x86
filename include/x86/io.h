0830729c6e75ec8f2ef734a3641aab6e2155123c
Arjob Mukherjee <arjobmukherjee@gmail.com>
2024-10-22 03:06:41 +0530

    New ioread() inline function for easier IO reads

--- include/x86/io.h
+++ include/x86/io.h
@@ -14,10 +14,18 @@
 #define IO_H_X86
 
 #include <buildcheck.h>
+#include <types.h>
 
 #define outb(p,v) __asm__ volatile ("out dx, al;" : :"a" (v), "d" (p))
 #define inb(p,v)  __asm__ volatile ("in al, dx" :"=a" (v) :"d" (p))
 // Reference: https://wiki.osdev.org/Inline_Assembly/Examples#IO_WAIT
 #define io_delay() outb(0x80, 0)
+
+static inline U8 ioread (INT port)
+{
+    U8 v;
+    inb (port, v);
+    return v;
+}
 
 #endif // IO_H_X86
