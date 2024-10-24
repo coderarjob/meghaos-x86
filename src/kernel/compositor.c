530347b9578c3bb64949efa5192d197d91ca7664
Arjob Mukherjee <arjobmukherjee@gmail.com>
2024-10-25 00:39:46 +0530

    Fix: Window UI colors in 8 bpp mode

--- src/kernel/compositor.c
+++ src/kernel/compositor.c
@@ -31,16 +31,20 @@ typedef struct Window {
 } Window;
 
 #if CONFIG_GXMODE_BITSPERPIXEL == 8
+    #define COLOR_WHITE               (15)
     #define WINDOW_TITLE_BAR_BG_COLOR (126)
     #define WINDOW_TITLE_BAR_FG_COLOR (15)
     #define WINDOW_BG_COLOR           (29)
-    #define WINDOW_BORDER_COLOR       (26)
+    #define WINDOW_BORDER_COLOR1      (26)
+    #define WINDOW_BORDER_COLOR2      (30)
     #define DESKTOP_BG_COLOR          (123)
 #elif CONFIG_GXMODE_BITSPERPIXEL == 32 || CONFIG_GXMODE_BITSPERPIXEL == 24
+    #define COLOR_WHITE               (0xFFFFFF)
     #define WINDOW_TITLE_BAR_BG_COLOR (0x003971)
     #define WINDOW_TITLE_BAR_FG_COLOR (0xFFFFFF)
     #define WINDOW_BG_COLOR           (0xDFDFDF)
-    #define WINDOW_BORDER_COLOR       (0xAAAAAA)
+    #define WINDOW_BORDER_COLOR1      (0xAAAAAA)
+    #define WINDOW_BORDER_COLOR2      (0xF2F2F2)
     #define DESKTOP_BG_COLOR          (0x007155)
 #endif
 
@@ -81,7 +85,7 @@ static void drawWindowDecorations (KGraphicsArea* wa, char* title)
 
     // Title bar bottom border
     y = WINDOW_TITLE_BAR_HEIGHT_PX;
-    kgraphics_hline (wa, x - 1, y - 1, w, 1, 0xFFFFFF);
+    kgraphics_hline (wa, x - 1, y - 1, w, 1, COLOR_WHITE);
 
     // ===============================
     // 3D Border
@@ -90,8 +94,8 @@ static void drawWindowDecorations (KGraphicsArea* wa, char* title)
     y = WINDOW_TITLE_BAR_TOP_PX;
     w = wa->width_px;
     h = wa->height_px;
-    kgraphics_inborder (wa, x, y, w, h, WINDOW_BORDER_WIDTH_PX, WINDOW_BORDER_COLOR);
-    kgraphics_inborder (wa, x + 1, y + 1, w - 2, h - 2, 1, 0xF2F2F2);
+    kgraphics_inborder (wa, x, y, w, h, WINDOW_BORDER_WIDTH_PX, WINDOW_BORDER_COLOR1);
+    kgraphics_inborder (wa, x + 1, y + 1, w - 2, h - 2, 1, WINDOW_BORDER_COLOR2);
 
     // ===============================
     // Active area
