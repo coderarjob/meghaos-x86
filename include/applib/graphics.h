10addc2d8de0d63ae488dbfb9e9931bc2fe76c8e
Arjob Mukherjee <arjobmukherjee@gmail.com>
2024-10-31 20:47:51 +0530

    Application space graphics functions

---
+++ include/applib/graphics.h
@@ -0,0 +1,51 @@
+/*
+ * --------------------------------------------------------------------------------------------------
+ * Megha Operating System V2 - Cross Platform Kernel - Kernel graphics header
+ * --------------------------------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#ifndef GRAPHICS_MODE_ENABLED
+    #error "Gui demo only works in graphics mode"
+#endif
+
+#include <types.h>
+#ifdef KERNEL
+    #include <applib/osif.h>
+#else
+    #include <osif.h>
+#endif
+
+// This is a arch independent color type large enough for all the color modes supported.
+typedef U32 Color;
+typedef OSIF_WindowFrameBufferInfo GraphicsContext;
+
+void graphics_rect (const GraphicsContext* g, UINT x, UINT y, UINT w, UINT h, Color color);
+void graphics_image_ppm (const GraphicsContext* g, UINT x, UINT y, UINT w, UINT h,
+                         UINT bytesPerPixel, U8* bytes);
+void graphics_drawfont (const GraphicsContext* g, UINT x, UINT y, UCHAR a, Color fg, Color bg);
+void graphics_putpixel (const GraphicsContext* g, UINT x, UINT y, Color color);
+void kgraphics_blit (const GraphicsContext* destg, UINT x, UINT y, const GraphicsContext* srcg);
+
+static inline void kgraphics_hline (const GraphicsContext* g, UINT x, UINT y, UINT w, UINT th,
+                                    Color color)
+{
+    graphics_rect (g, x, y, w, th, color);
+}
+
+static inline void kgraphics_vline (const GraphicsContext* g, UINT x, UINT y, UINT h, UINT th,
+                                    Color color)
+{
+    graphics_rect (g, x, y, th, h, color);
+}
+
+/* Draws rectangle with borders on the inside of a rectangle */
+static inline void kgraphics_inborder (const GraphicsContext* g, UINT x, UINT y, UINT w, UINT h,
+                                       UINT th, Color color)
+{
+    kgraphics_hline (g, x, y, w, th, color);            // Top
+    kgraphics_vline (g, x, y, h, th, color);            // Left
+    kgraphics_vline (g, (x + w - th), y, h, th, color); // Right
+    kgraphics_hline (g, x, (y + h - th), w, th, color); // Bottom
+}
---
+++ src/apps/applib/graphics.c
@@ -0,0 +1,114 @@
+/*
+ * --------------------------------------------------------------------------------------------------
+ * Megha Operating System V2 -  App library - Graphics
+ * --------------------------------------------------------------------------------------------------
+ */
+
+#include <kstdlib.h>
+#include <types.h>
+#include <applib/graphics.h>
+#include <applib/app.h>
+
+typedef U8 IndexColor8Bit;
+
+// RGB is BGR in memory and that is the order in which the fields apprear. This simplifies when
+// assignning/copying color values from memory (like in graphics_rect, graphics_drawfont etc).
+// Note: PPM and RAW image formats etc, which store RGB however store color in R,G,B and not B,G,R.
+typedef struct RGBColor24Bits {
+    U8 blue;
+    U8 green;
+    U8 red;
+} __attribute__ ((packed)) RGBColor24Bits;
+
+// ARGB is BGRA in memory and that is the order in which the fields apprear. This simplifies when
+// assignning/copying color values from memory (like in graphics_rect, graphics_drawfont etc).
+// Note: PPM and RAW image formats etc, which store RGB however store color in R,G,B and not B,G,R.
+typedef struct RGBColor32Bits {
+    U8 blue;
+    U8 green;
+    U8 red;
+    U8 alpha;
+} __attribute__ ((packed)) RGBColor32Bits;
+
+#if GRAPHICS_BPP == 8
+typedef IndexColor8Bit GxColor;
+#elif GRAPHICS_BPP == 24
+typedef RGBColor24Bits GxColor;
+#elif GRAPHICS_BPP == 32
+typedef RGBColor32Bits GxColor;
+#endif
+
+void graphics_blit (const GraphicsContext* destg, UINT x, UINT y, const GraphicsContext* srcg)
+{
+    FUNC_ENTRY ("area: %px, x: %u, y: %u, src area: %px", destg, x, y, srcg);
+
+    // k_assert (destg->bytesPerPixel == srcg->bytesPerPixel, "Graphics area mismatch");
+
+    SIZE bytesPerPixel = destg->bytesPerPixel;
+    SIZE bytesPerRow   = destg->bytesPerRow;
+    U8* start          = (U8*)destg->buffer + (y * bytesPerRow) + (x * bytesPerPixel);
+    UINT h             = srcg->height_px;
+    UINT w             = srcg->width_px;
+    U8* bytes          = srcg->buffer;
+
+    for (; h > 0; h--) {
+        GxColor* row = (GxColor*)start;
+        for (UINT lw = w; lw > 0; lw--, row++, bytes += bytesPerPixel) {
+            *row = *(GxColor*)bytes;
+        }
+        start += bytesPerRow;
+    }
+}
+
+void graphics_image_ppm (const GraphicsContext* g, UINT x, UINT y, UINT w, UINT h,
+                         UINT bytesPerPixel, U8* bytes)
+{
+    FUNC_ENTRY ("area: %px, x: %u, y: %u, w: %u, h: %u, bytes: %px", g, x, y, w, h, bytes);
+
+    SIZE bytesPerRow = g->bytesPerRow;
+    U8* start        = (U8*)g->buffer + (y * bytesPerRow) + (x * g->bytesPerPixel);
+
+    for (; h > 0; h--) {
+        GxColor* row = (GxColor*)start;
+        for (UINT lw = w; lw > 0; lw--, row++, bytes += bytesPerPixel) {
+#if CONFIG_GXMODE_BITSPERPIXEL == 32 || CONFIG_GXMODE_BITSPERPIXEL == 24
+    #if CONFIG_GXMODE_BITSPERPIXEL == 32
+            row->alpha = 0;
+    #endif
+            row->red   = *bytes;
+            row->green = *(bytes + 1);
+            row->blue  = *(bytes + 2);
+#elif CONFIG_GXMODE_BITSPERPIXEL == 8
+            *row = *bytes;
+#endif
+        }
+        start += bytesPerRow;
+    }
+}
+
+void graphics_putpixel (const GraphicsContext* g, UINT x, UINT y, Color color)
+{
+    FUNC_ENTRY ("area: %px, x: %u, y: %u, color: %x", g, x, y, color);
+
+    GxColor* start = (GxColor*)(g->buffer + (y * g->bytesPerRow) + (x * g->bytesPerPixel));
+    GxColor* col   = (GxColor*)&color;
+    *start         = *col;
+}
+
+void graphics_rect (const GraphicsContext* g, UINT x, UINT y, UINT w, UINT h, Color color)
+{
+    FUNC_ENTRY ("area: %px, x: %u, y: %u, w: %u, h: %u, color: %x", g, x, y, w, h, color);
+
+    SIZE bytesPerRow = g->bytesPerRow;
+    U8* start        = (U8*)g->buffer + (y * bytesPerRow) + (x * g->bytesPerPixel);
+
+    GxColor* col = (GxColor*)&color;
+
+    for (; h > 0; h--) {
+        GxColor* row = (GxColor*)start;
+        for (UINT lw = w; lw > 0; lw--, row++) {
+            *row = *col;
+        }
+        start += bytesPerRow;
+    }
+}
