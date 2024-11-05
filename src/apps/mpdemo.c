b2285a0106f74ecede960204b9ee6aea98ffe819
Arjob Mukherjee <arjobmukherjee@gmail.com>
2024-11-05 12:47:04 +0530

    mpdemo app now uses applib delay() function

--- src/apps/mpdemo.c
+++ src/apps/mpdemo.c
@@ -29,18 +29,6 @@ static void s_progressbar (UINT iterPerStep, char* title, UINT row, UINT color);
 static void thread0();
 static void thread1();
 
-#define HANDLE_YIELD_REQ_EVENT
-
-#ifdef HANDLE_YIELD_REQ_EVENT
-static bool shouldYield()
-{
-    UINT pid = os_process_get_pid();
-    OSIF_ProcessEvent e = {0};
-    os_process_pop_event (pid, &e);
-    return (e.event == OSIF_PROCESS_EVENT_PROCCESS_YIELD_REQ);
-}
-#endif
-
 static void s_printString (U32 row, U32 col, U32 bgcolor, U32 fgcolor, char* text)
 {
     syscall (OSIF_SYSCALL_CONSOLE_SETCOLOR, bgcolor, fgcolor, 0, 0, 0);
@@ -48,25 +36,15 @@ static void s_printString (U32 row, U32 col, U32 bgcolor, U32 fgcolor, char* tex
     os_putstr (text);
 }
 
-void s_progressbar (UINT iterPerStep, char* title, UINT row, UINT color)
+void s_progressbar (UINT delay_ms, char* title, UINT row, UINT color)
 {
     s_printString (row, 0, BLACK, DARK_GRAY, title);
 
-    UINT column = 0;
-    UINT max    = iterPerStep * MAX_VGA_COLUMNS;
+    UINT max = MAX_VGA_COLUMNS;
 
     for (UINT i = 0; i < max; i++) {
-        s_printString (row + 2, column, color, DARK_GRAY, " ");
-        if (i % iterPerStep == 0) {
-            column = (column + 1) % MAX_VGA_COLUMNS;
-        }
-#ifdef HANDLE_YIELD_REQ_EVENT
-        if (shouldYield()) {
-            os_yield();
-        }
-#else
-        os_yield();
-#endif
+        s_printString (row + 2, i, color, DARK_GRAY, " ");
+        delay (delay_ms);
     }
 }
 
@@ -75,9 +53,7 @@ void proc_main()
     os_thread_create (thread0, false);
     os_thread_create (&thread1, false);
 
-    UINT iterPerStep = 110;
-
-    s_progressbar (iterPerStep, "Process 0:\n", 26, RED);
+    s_progressbar (200, "Process 0:\n", 26, RED);
 
     os_process_kill();
     s_printString (37, 0, BLACK, WHITE,
@@ -86,16 +62,12 @@ void proc_main()
 
 void thread0()
 {
-    UINT iterPerStep = 10;
-
-    s_progressbar (iterPerStep, "Thread 0:\n", 30, GREEN);
+    s_progressbar (100, "Thread 0:\n", 30, GREEN);
     os_process_kill();
 }
 
 void thread1()
 {
-    UINT iterPerStep = 7;
-
-    s_progressbar (iterPerStep, "Thread 1:\n", 34, YELLOW);
+    s_progressbar (50, "Thread 1:\n", 34, YELLOW);
     os_process_kill();
 }
