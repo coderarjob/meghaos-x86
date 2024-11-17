c7cee9a9be7b9de033054b7fd86c4c81c30db28a
Arjob Mukherjee <arjobmukherjee@gmail.com>
2024-11-17 10:30:45 +0530

    kpanic now shows last Kernel error
    Also includes:
    * UI of graphical panic changed slightly.
    * kpanic_gx_bsod renamed kpanic_gx_ui.

--- src/kernel/kpanic.c
+++ src/kernel/kpanic.c
@@ -11,6 +11,7 @@
 #include <disp.h>
 #include <stdarg.h>
 #include <kernel.h>
+#include <utils.h>
 #ifdef GRAPHICS_MODE_ENABLED
     #include <graphics.h>
     #include <guicolours.h>
@@ -18,16 +19,17 @@
 
 static void arch_dump_call_trace (PTR* raddrs, INT count);
 #ifdef GRAPHICS_MODE_ENABLED
-static void kpanic_gx_bsod (const char* buffer);
+static void kpanic_gx_ui (const char* buffer);
 #endif
 
 #ifdef GRAPHICS_MODE_ENABLED
-static void kpanic_gx_bsod (const char* buffer)
+static void kpanic_gx_ui (const char* buffer)
 {
     if (KERNEL_PHASE_CHECK (KERNEL_PHASE_STATE_GRAPHICS_READY)) {
         KGraphicsArea* ga = (KGraphicsArea*)&g_kstate.gx_hwfb;
-        graphics_rect (ga, 0, 0, ga->width_px, ga->height_px, COLOR_BG_BSOD);
-        kgraphics_drawstring (ga, 10, 10, buffer, COLOR_WHITE, COLOR_BG_BSOD);
+        UINT width        = MIN (ga->width_px, CONFIG_GXMODE_FONT_WIDTH * MAX_VGA_COLUMNS);
+        graphics_rect (ga, 0, 0, width, ga->height_px, COLOR_BG_BSOD);
+        kgraphics_drawstring (ga, 10, 0, buffer, COLOR_WHITE, COLOR_BG_BSOD);
     } else {
         // This has to be a failure very early on.
         // Not sure what can we do here!!
@@ -46,7 +48,9 @@ void kpanic_ndu (UINT line, const CHAR* file, const CHAR* fmt, ...)
     INT len = 0;
 
     len = kearly_snprintf (buffer, ARRAY_LENGTH (buffer), "\n\nKernel Panic:");
-    len += kearly_snprintf (buffer + len, ARRAY_LENGTH (buffer), "\nAt: %s: %u\n\n", file, line);
+    len += kearly_snprintf (buffer + len, ARRAY_LENGTH (buffer), "\nAt: %s: %u", file, line);
+    len += kearly_snprintf (buffer + len, ARRAY_LENGTH (buffer), "\nKernel error: %x\n\n",
+                            g_kstate.errorNumber);
 
     va_start (l, fmt);
     len += kearly_vsnprintf (buffer + len, ARRAY_LENGTH (buffer), fmt, l);
@@ -59,7 +63,7 @@ void kpanic_ndu (UINT line, const CHAR* file, const CHAR* fmt, ...)
     len += kearly_snprintf (buffer + len, ARRAY_LENGTH (buffer), "\n------------\n");
 
 #ifdef GRAPHICS_MODE_ENABLED
-    kpanic_gx_bsod (buffer);
+    kpanic_gx_ui (buffer);
 #endif
 
 #ifdef DEBUG
