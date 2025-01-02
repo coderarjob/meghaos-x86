7a550d0cb3f6ae80ae0e010fd9ee94c0f664a0ca
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-01-03 03:50:08 +0530

    ut: Reuse kstdlib tests for libcm string functions

--- src/unittests/kernel/kstdlib_test.c
+++ src/unittests/kernel/kstdlib_test.c
@@ -1,12 +1,28 @@
 #include <stdbool.h>
 #include <types.h>
-#include <kstdlib.h>
 #include <utils.h>
 #include <unittest/unittest.h>
-#include <mock/kernel/paging.h>
 
-extern bool panic_invoked;
+#ifdef LIBCM
+    #include <cm/cm.h>
 
+    #define MEMSET_FN_UNDER_TEST cm_memset
+    #define MEMCPY_FN_UNDER_TEST cm_memcpy
+    #define STRLEN_FN_UNDER_TEST cm_strlen
+    #define STRCPY_FN_UNDER_TEST cm_strncpy
+#else
+    #include <kstdlib.h>
+    #include <mock/kernel/paging.h>
+
+    #define MEMSET_FN_UNDER_TEST k_memset
+    #define MEMCPY_FN_UNDER_TEST k_memcpy
+    #define STRLEN_FN_UNDER_TEST k_strlen
+    #define STRCPY_FN_UNDER_TEST k_strncpy
+#endif
+
+#ifndef LIBCM
+// The corresponding functions are not available in libcm. So following tests are only for the
+// Kernel.
 TEST (MEM, memcmp_one_byte)
 {
     U8 dest[1] = { 10 };
@@ -40,49 +56,6 @@ TEST (MEM, memcmp_mismatch_within_range)
     U8 src[7]  = { 1, 2, 3, 4, 5, 6, 7 };
 
     EQ_SCALAR (k_memcmp (dest, src, 7), false);
-    END();
-}
-
-TEST(MEM, memset_one_byte)
-{
-    U8 dest;
-    EQ_SCALAR((PTR)k_memset (&dest, 0x1A, 1), (PTR)&dest);
-    EQ_SCALAR(dest, 0x1A);
-    END();
-}
-
-TEST(STRING, string_length)
-{
-    EQ_SCALAR(k_strlen(""), 0);
-    EQ_SCALAR(k_strlen("ABCD"), 4);
-    EQ_SCALAR(k_strlen("A\n\tB"), 4);
-    END();
-}
-
-TEST(STRING, string_copy)
-{
-    char destination[10];
-    char *source = NULL;
-
-    // Copy empty string
-    source = "";
-    EQ_SCALAR((PTR)k_strncpy(destination,source,ARRAY_LENGTH(destination)), (PTR)destination);
-    EQ_STRING(source, destination);
-
-    // Copy < size of destination
-    source = "ABC";
-    EQ_SCALAR((PTR)k_strncpy(destination,source,ARRAY_LENGTH(destination)), (PTR)destination);
-    EQ_STRING(source, destination);
-
-    //// Copy = size of destination
-    source = "123456789";
-    EQ_SCALAR((PTR)k_strncpy(destination,source,ARRAY_LENGTH(destination)), (PTR)destination);
-    EQ_STRING(source, destination);
-
-    //// Copy > size of destination
-    //source = "123456789ABCD";
-    EQ_SCALAR((PTR)k_strncpy(destination,source,ARRAY_LENGTH(destination)), (PTR)destination);
-    EQ_STRING("123456789", destination); // Not more than size of destination is copied.
     END();
 }
 
@@ -124,6 +97,54 @@ TEST (MEMSET_PAT4, memset_pat4_4_bytes_pat)
 
     END();
 }
+#endif
+
+TEST(MEM, memset_one_byte)
+{
+    U8 dest;
+    EQ_SCALAR((PTR)MEMSET_FN_UNDER_TEST (&dest, 0x1A, 1), (PTR)&dest);
+    EQ_SCALAR(dest, 0x1A);
+    END();
+}
+
+TEST(STRING, string_length)
+{
+    EQ_SCALAR(STRLEN_FN_UNDER_TEST(""), 0);
+    EQ_SCALAR(STRLEN_FN_UNDER_TEST("ABCD"), 4);
+    EQ_SCALAR(STRLEN_FN_UNDER_TEST("A\n\tB"), 4);
+    END();
+}
+
+TEST (STRING, string_copy)
+{
+    char destination[10];
+    char* source = NULL;
+
+    // Copy empty string
+    source = "";
+    EQ_SCALAR ((PTR)STRCPY_FN_UNDER_TEST (destination, source, ARRAY_LENGTH (destination)),
+               (PTR)destination);
+    EQ_STRING (source, destination);
+
+    // Copy < size of destination
+    source = "ABC";
+    EQ_SCALAR ((PTR)STRCPY_FN_UNDER_TEST (destination, source, ARRAY_LENGTH (destination)),
+               (PTR)destination);
+    EQ_STRING (source, destination);
+
+    //// Copy = size of destination
+    source = "123456789";
+    EQ_SCALAR ((PTR)STRCPY_FN_UNDER_TEST (destination, source, ARRAY_LENGTH (destination)),
+               (PTR)destination);
+    EQ_STRING (source, destination);
+
+    //// Copy > size of destination
+    // source = "123456789ABCD";
+    EQ_SCALAR ((PTR)STRCPY_FN_UNDER_TEST (destination, source, ARRAY_LENGTH (destination)),
+               (PTR)destination);
+    EQ_STRING ("123456789", destination); // Not more than size of destination is copied.
+    END();
+}
 
 TEST(MEM, memcpy_overlap)
 {
@@ -139,7 +160,7 @@ TEST(MEM, memcpy_overlap)
     CHAR *src = &memory[0];
     CHAR *dest = &memory[5];
 
-    EQ_SCALAR((PTR)k_memcpy (dest, src, 10), (PTR)dest);
+    EQ_SCALAR((PTR)MEMCPY_FN_UNDER_TEST (dest, src, 10), (PTR)dest);
 
     for (int i = 0; i < 10; i++)
         EQ_SCALAR(dest[i], i + 1);              // Dest[0 to 9] has 1 to 10
@@ -157,7 +178,7 @@ TEST(MEM, memcpy_normal)
     CHAR *src = "123456789ABCDEF0";
     CHAR dest[10];
 
-    EQ_SCALAR((PTR)k_memcpy (dest, src, ARRAY_LENGTH(dest)), (PTR)&dest);
+    EQ_SCALAR((PTR)MEMCPY_FN_UNDER_TEST (dest, src, ARRAY_LENGTH(dest)), (PTR)&dest);
 
     for (int i = 0; i < ARRAY_LENGTH(dest); i++)
         EQ_SCALAR(dest[i], src[i]);
@@ -169,7 +190,7 @@ TEST(MEM, memset_normal)
 {
     U8 dest[10];
 
-    EQ_SCALAR((PTR)k_memset (dest, 0x1A, ARRAY_LENGTH(dest)), (PTR)&dest);
+    EQ_SCALAR((PTR)MEMSET_FN_UNDER_TEST (dest, 0x1A, ARRAY_LENGTH(dest)), (PTR)&dest);
 
     for (int i = 0; i < ARRAY_LENGTH(dest); i++)
         EQ_SCALAR(dest[i], 0x1A);
@@ -179,7 +200,9 @@ TEST(MEM, memset_normal)
 
 void reset()
 {
+#ifndef LIBCM
     resetPagingFake();
+#endif
 }
 
 int main()
@@ -188,13 +211,15 @@ int main()
     memset_normal();
     memcpy_normal();
     memcpy_overlap();
+#ifndef LIBCM
     memset_pat4_2_bytes_pat();
     memset_pat4_3_bytes_pat();
     memset_pat4_4_bytes_pat();
-    string_length();
-    string_copy();
     memcmp_one_byte();
     memcmp_empty();
     memcmp_mismatch_outside_range();
     memcmp_mismatch_within_range();
+#endif
+    string_length();
+    string_copy();
 }
