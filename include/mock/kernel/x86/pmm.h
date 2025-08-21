f3b6f43c79c6ba6eae14133f5aa0936dacc1466e
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-08-21 19:57:16 +0530

    UT: pmm_test now using yukti.h

--- include/mock/kernel/x86/pmm.h
+++ include/mock/kernel/x86/pmm.h
@@ -1,8 +1,9 @@
 #ifndef PMM_FAKE_X86_H
 #define PMM_FAKE_X86_H
 
+#define YUKTI_TEST_STRIP_PREFIX
+#include <unittest/yukti.h>
 #include <bitmap.h>
-#include <unittest/fake.h>
 #include <types.h>
 
 DECLARE_FUNC_VOID(kpmm_arch_init, Bitmap *);
