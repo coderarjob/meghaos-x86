4ce065c09cf7c44d8c276e46df062c67cf6d223c
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-08-21 20:05:14 +0530

    UT: Paging test now using yukti.h

--- src/unittests/kernel/paging_test.c
+++ src/unittests/kernel/paging_test.c
@@ -1,23 +1,26 @@
+#define YUKTI_TEST_STRIP_PREFIX
+#define YUKTI_TEST_IMPLEMENTATION
+#include <unittest/yukti.h>
 #include <paging.h>
-#include <unittest/unittest.h>
 
 TEST (paging, pageframe_to_physical) {
-    EQ_SCALAR(PAGEFRAME_TO_PHYSICAL(0x3), 0x3 * CONFIG_PAGE_FRAME_SIZE_BYTES);
-    EQ_SCALAR(PAGEFRAME_TO_PHYSICAL(0x0), 0x0);
+    EQ_SCALAR(PAGEFRAME_TO_PHYSICAL(0x3), 0x3U * CONFIG_PAGE_FRAME_SIZE_BYTES);
+    EQ_SCALAR(PAGEFRAME_TO_PHYSICAL(0x0), 0x0U);
     END();
 }
 
 TEST (paging, physical_to_pageframe) {
-    EQ_SCALAR(PHYSICAL_TO_PAGEFRAME(0x0), 0x0);
-    EQ_SCALAR(PHYSICAL_TO_PAGEFRAME(4 * CONFIG_PAGE_FRAME_SIZE_BYTES), 0x4);
+    EQ_SCALAR(PHYSICAL_TO_PAGEFRAME(0x0), 0x0U);
+    EQ_SCALAR(PHYSICAL_TO_PAGEFRAME(4 * CONFIG_PAGE_FRAME_SIZE_BYTES), 0x4U);
     END();
 }
 
-void reset() {
+void yt_reset() {
 }
 
 int main() {
+    YT_INIT();
     pageframe_to_physical();
     physical_to_pageframe();
-    return 0;
+    RETURN_WITH_REPORT();
 }
