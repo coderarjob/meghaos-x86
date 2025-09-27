0954e9da5713d062c0ed67bc6d9ef3bb72f2553b
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-09-28 01:14:55 +0530

    Using ZM module to display message

--- src/apps/zapps/hello/build.zig
+++ src/apps/zapps/hello/build.zig
@@ -3,11 +3,17 @@ const mos = @import("mos_build.zig");
 
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
+    const target = b.resolveTargetQuery(mos.i686_target_query);
+    const options = try mos.addDefaultOptions(b);
     const exe = mos.addExecutable(b, "hello", .{
         .optimize = optimize,
         .root_src_file = "./main.zig",
-        .options = try mos.addDefaultOptions(b),
-        .target = b.resolveTargetQuery(mos.i686_target_query),
+        .options = options,
+        .target = target,
     });
+
+    const zm = b.dependency("zm", .{}).module("zm");
+    mos.addDefaultDependencies(b, zm, options);
+    exe.compilation_step.root_module.addImport("zm",zm);
     b.getInstallStep().dependOn(exe.root_step);
 }
--- src/apps/zapps/hello/main.zig
+++ src/apps/zapps/hello/main.zig
@@ -1,23 +1,10 @@
 const std = @import("std");
-const cm = @cImport({
-    @cInclude("cm.h");
-});
-pub const panic = std.debug.FullPanic(crash);
-
-const text = "Test application";
-
-fn crash(_: []const u8, _: ?usize) noreturn {
-    cm.cm_process_abort(cm.CM_ABORT_EXIT_CODE);
-    while(true) {}
-}
-
-fn get_length() usize {
-    return 500;
-}
+const zm = @import("zm");
 
 export fn proc_main() void {
-    const data = text;
-    const a = data[get_length()];
-    //@panic("Hello");
-    cm.cm_process_kill(a);
+    zm.debug.console_writeline("\n\n");
+    for (0..9) |_| {
+        zm.debug.console_writeline("Hello from Zig!!\n");
+    }
+    zm.process.exit(1);
 }
