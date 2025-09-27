59b929579e8ab2724b44eb850d274db88a0bde4d
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-09-27 09:36:20 +0530

    Move hello app inside its own folder
    * As a consequence of this, the build.zig now could not import the
      common 'mos_build.zig' file as will complain about including a
      non-project file. The quick solution is to just provide a symbolic
      link for this 'external file', this works but its not the most
      idiomatic or cross platform.

--- src/apps/zapps/hello/build.zig
+++ src/apps/zapps/hello/build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const exe = mos.addExecutable(b, "hello", .{
         .optimize = optimize,
-        .root_src_file = "./hello.zig",
+        .root_src_file = "./main.zig",
         .options = try mos.addDefaultOptions(b),
         .target = b.resolveTargetQuery(mos.i686_target_query),
     });
