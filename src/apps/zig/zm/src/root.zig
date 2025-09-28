0e8a62bb183ec7a2694a8b80814d9c6d5c742e76
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-09-29 00:35:00 +0530

    zm: Added wrapper for syscalls, graphics and windows

--- src/apps/zig/zm/src/root.zig
+++ src/apps/zig/zm/src/root.zig
@@ -1,5 +1,9 @@
 pub const debug = @import("debug.zig");
 pub const process = @import("process.zig");
+pub const syscall = @import("syscalls.zig");
+pub const window = @import("window.zig");
+pub const graphics = @import("graphics.zig");
+
 pub const MosApplicationPanic = @import("std").debug.FullPanic(crash);
 
 fn crash(_: []const u8, _: ?usize) noreturn {
